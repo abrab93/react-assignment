ba5078dbe622139daee1ba8594cb4da1093bbcf3
abrab93 <abrab93@gmail.com>
2019-11-01 15:37:18 +0000

    c9

--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ deleteLetterHandler = (index) =>{
     const newTextInput = letters.join('')
 
     this.setState({
-        textInput : newTextInput
+        textInput : newTextInput,
+        textLength : newTextInput.length
     })
 }
 
@@ -66,7 +67,7 @@ deleteLetterHandler = (index) =>{
           <li>*Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
           <li>*Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
           <li>*Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+          <li>*When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
       </div>
