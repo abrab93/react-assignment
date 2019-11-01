4a3cd22d861123aa78be84cf1a8a2507e38ac92a
abrab93 <abrab93@gmail.com>
2019-11-01 15:09:04 +0000

    c4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ValidationComponent from './Validation/ValidationComponent'
+import CharComponent from './Char/CharComponent';
 
 class App extends Component {
 
 state = {
-    textLength : 0
+    textLength : 0,
+    textInput : ''
 }
 /*
 Bonjour M;
@@ -15,23 +17,40 @@ En attendant une réponse favorable de votre part, veuillez accepter monsieur me
 */
 
 changeInputTextHandler = (event) =>{
-    this.setState({textLength : event.target.value.length});
+    this.setState({
+        textLength : event.target.value.length,
+        textInput  :  event.target.value
+    });
 }
 
   render() {
+
+      let letters = (
+          <div>
+            {
+                this.state.textInput.split('').map(char =>{
+                    return <CharComponent letter={char} />
+                })
+            }
+          </div>
+      );
+
+
+
     return (
       <div className="App">
 
         <input onChange={this.changeInputTextHandler} ></input>
         <p>Text length : {this.state.textLength}</p>
         <ValidationComponent textLegth={this.state.textLength}/>
+        {letters}
         <br/>
         <br/>
         <ol>
           <li>*Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
           <li>*Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li>*Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>*Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
--- src/Char/CharComponent.js
+++ src/Char/CharComponent.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const charComponent = () =>{
-    return (
-        <div>
+const charComponent = (props) =>{
 
+   const style = {
+        display: 'inline-block',
+        padding: '16px',
+        textAlign: 'center',
+        margin: '16px',
+        border: '1px solid black'
+    }
+
+    return (
+        <div style={style}>
+            {props.letter}
         </div>
     )
 }
