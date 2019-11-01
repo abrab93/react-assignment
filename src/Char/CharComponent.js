98893bfc79def6266a01e3909681ada70a78f78f
abrab93 <abrab93@gmail.com>
2019-11-01 15:23:25 +0000

    c5

--- src/Char/CharComponent.js
+++ src/Char/CharComponent.js
@@ -11,7 +11,7 @@ const charComponent = (props) =>{
     }
 
     return (
-        <div style={style}>
+        <div style={style} onClick={props.click}>
             {props.letter}
         </div>
     )
